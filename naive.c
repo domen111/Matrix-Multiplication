3e7d0601e3c4be0b07d01d05fc452507836d8288
domen111 <domen111@gmail.com>
2017-12-15 16:32:20 +0800

    naive

--- naive.c
+++ naive.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-#define max 8000
-double A[max][max] = {};
-double B[max][max] = {};
-double C[max][max] = {};
-	
+#include "matrix.h"
 int main(){
-	for(int i = 0; i < max; i++)
-		for(int j = 0; j < max; j++)
-			A[i][j] = 1.00, B[i][j] = 1.00, C[i][j] = 0.00;
-	for(int i = 0; i < max; i++){
-		for(int j = 0; j < max; j++){
-			for(int k = 0; k < max; k++)
-				C[i][k] += A[i][j]*B[j][k];
+	load();
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			for(int k = 0; k < n; k++)
+				C[i*n+k] += A[i*n+j]*B[j*n+k];
 		}
 	}
-	for(int i = 0; i < max; i++){
-		for(int j = 0; j < max; j++)
-			printf("%lf ", C[i][j]);
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++)
+			printf("%lf ", C[i*n+j]);
 		puts("");
 	}
 	return 0;
